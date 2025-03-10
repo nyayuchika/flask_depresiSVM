84c9513b8f9c2b11e094c2a2df8fb296c675f49a
Nyayu Chika Marselina <nyayuchikamsln@gmail.com>
2025-03-10 13:34:41 +0700

    update code

--- deploy.py
+++ deploy.py
@@ -3,7 +3,7 @@ import pickle
 
 app = Flask(__name__)
 #load the model
-model = pickle.load(open('savedmodel.sav', 'rb'))
+model = pickle.load(open('depresimlmodel.sav', 'rb'))
 
 @app.route('/')
 def home():
@@ -13,11 +13,8 @@ def home():
 
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    sepal_length = float(request.form['sepal_length'])
-    sepal_width = float(request.form['sepal_width'])
-    petal_length = float(request.form['petal_length'])
-    petal_width = float(request.form['petal_width'])
-    result = model.predict([[sepal_length,sepal_width,petal_length,petal_width]])[0]
+    text_in = str(request.form['text_in'])
+    result = model.predict([[text_in]])[0]
     return render_template('./index.html', **locals())
 
 

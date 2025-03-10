4da9f5914ba69a9e38ab910f32b78c286db852cc
Nyayu Chika Marselina <nyayuchikamsln@gmail.com>
2025-03-10 14:40:15 +0700

    running, but still 0

--- deploy.py
+++ deploy.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request
 import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.svm import SVC
 
 app = Flask(__name__)
 #load the model
 model = pickle.load(open('depresimlmodel.sav', 'rb'))
+vectorizer = pickle.load(open('vectorizer.sav', 'rb'))
+
+
+# import os
+# print(os.lisdtdir())
 
 @app.route('/')
 def home():
@@ -13,8 +20,13 @@ def home():
 
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    text_in = str(request.form['text_in'])
-    result = model.predict([[text_in]])[0]
+    text_input = request.form.get('text_input','').strip()
+    text_transformed = vectorizer.transform([text_input])
+    #cek dulu gaiss
+    print(text_transformed.toarray())
+
+    
+    result = model.predict(text_transformed)[0]
     return render_template('./index.html', **locals())
 
 
